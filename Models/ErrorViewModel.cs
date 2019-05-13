bd4ea8d35d9c0e0bef86d7be8d84744be2793ba8
Zayne  Berndt <berndt.zayne@gmail.com>
2019-05-13 13:24:42 +0100

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace sampleCliApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
